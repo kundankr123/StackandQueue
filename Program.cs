0521920a51e0153a222c7abd4f61e37a55a15630
kundan <goswamikundan123@gmail.com>
2023-02-26 20:17:31 +0530

    Adding file which creates the queue and displays them.

--- Program.cs
+++ Program.cs
@@ -6,24 +6,35 @@
         {
             //Console.WriteLine("Welcome to Stack and Queue Master Branch.");
 
-            Stack<int> stack = new Stack<int>();
-            stack.Push(56);
-            stack.Push(30);
-            stack.Push(70);
+            //Stack<int> stack = new Stack<int>();
+            //stack.Push(56);
+            //stack.Push(30);
+            //stack.Push(70);
 
-            foreach (int ele in stack)
+            //foreach (int ele in stack)
+            //{
+            //    Console.WriteLine(ele);
+            //}
+
+            //while(stack.Count > 0)
+            //{
+            //    Console.WriteLine("****************");
+            //    Console.WriteLine("Top element : " + stack.Peek());
+            //    Console.WriteLine("****************");
+            //    stack.Pop();
+            //}
+
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            foreach (int ele in queue)
             {
                 Console.WriteLine(ele);
             }
 
-            while(stack.Count > 0)
-            {
-                Console.WriteLine("****************");
-                Console.WriteLine("Top element : " + stack.Peek());
-                Console.WriteLine("****************");
-                stack.Pop();
-            }
-            
         }
     }
 }
