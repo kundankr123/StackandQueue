4d8b49f80d81dbe7694ec073d3825d9f6a21b447
kundan <goswamikundan123@gmail.com>
2023-02-26 19:48:12 +0530

    Adding file which displays greeting message.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackandQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Master Branch.");
+        }
+    }
+}
